9259722131cab0b6fa429bb3286143f069d12c0e
ob-sc <ole.bergen.92@gmail.com>
2021-05-18 15:52:15 +0200

    zw mail

--- server/util/mail.js
+++ server/util/mail.js
@@ -1,21 +1,34 @@
 import nodemailer from 'nodemailer';
 
-const mail = async () => {
-  let transporter = nodemailer.createTransport({
+const mail = async (from = 'aushilfen', to, subject, text, html) => {
+  const transporter = nodemailer.createTransport({
     host: '192.168.100.50',
     port: 25,
     secure: false,
   });
 
   await transporter.sendMail({
-    from: 'STARCAR Onboarding <onboarding@starcar.de>',
-    to: 'ole.bergen@starcar.de',
-    subject: 'hi?',
-    text: 'hi',
-    html: '<span style="font-family: Arial, sans-serif;">hi</span>',
+    from:
+      from === 'aushilfen'
+        ? 'STARCAR Aushilfen <aushilfen@starcar.local>'
+        : from === 'onboarding'
+        ? 'STARCAR Onboarding <onboarding@starcar.local>'
+        : 'STARCAR <noreply@starcar.de>',
+    to,
+    subject,
+    text,
+    html,
   });
 };
 
 // in mail direkt link zu id
 
 export default mail;
+
+// await transporter.sendMail({
+//   from: 'STARCAR Onboarding <onboarding@starcar.de>',
+//   to: 'ole.bergen@starcar.de',
+//   subject: 'hi?',
+//   text: 'hi',
+//   html: '<span style="font-family: Arial, sans-serif;">hi</span>',
+// });
