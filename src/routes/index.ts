a26774697c50209c8bb6e935b9146b0140cbe54a
ob-sc <bergen@starcar.de>
2021-06-06 01:11:45 +0200

    auth fertig

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,9 @@
 import user from './user/route';
-import auth from './auth/route';
+import auth from './auth';
 import aushilfen from './aushilfen/route';
 import angemeldet from './angemeldet/route';
 import zeiten from './zeiten/route';
 import onboarding from './onboarding/route';
 import stationen from './stationen/route';
 
-export default {
-  user,
-  auth,
-  aushilfen,
-  angemeldet,
-  zeiten,
-  onboarding,
-  stationen,
-};
+export { user, auth, aushilfen, angemeldet, zeiten, onboarding, stationen };
