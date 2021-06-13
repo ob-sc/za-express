4f2c9aa3cd62d52cc1dc1fe608fd7ee4a32f672a
ob-sc <bergen@starcar.de>
2021-06-13 18:46:22 +0200

    json response type

--- src/middleware/response.ts
+++ src/middleware/response.ts
@@ -1,16 +1,20 @@
 import { RequestHandler } from 'express';
+import { JSONResponse } from '../../za-types/server/response';
 
 const response: RequestHandler = (req, res, next) => {
   res.okmsg = (data, code = 200) => {
-    res.status(code).json({
+    const r: JSONResponse = {
       message: typeof data === 'string' ? data : 'Anfrage erfolgreich',
       code,
       result: typeof data === 'object' ? data : {},
-    });
+    };
+
+    res.status(code).json(r);
   };
 
   res.errmsg = (message = 'Fehler bei Operation', code = 500, error) => {
-    res.status(code).json({ message, code, error });
+    const r: JSONResponse = { message, code, error };
+    res.status(code).json(r);
   };
 
   next();
